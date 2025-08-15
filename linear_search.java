e64c969a0799c838a9de1d7a3b9fec3d34610f9c
Srujal Sau <srujalsau@gmail.com>
2025-08-15 21:01:38 +0530

    implement linear search algorithm for integer and character arrays

---
+++ linear_search.java
@@ -0,0 +1,23 @@
+public class linear_search {
+    public static void main (String[] args){
+        int [] arr = {10,23,45,12,1,34,56};
+        int target= 23;
+        boolean search= l_search(arr, target);
+        System.out.println(search);      
+        
+    }
+    static boolean l_search (int arr[], int target ){
+        if (arr.length==0){
+            return false;
+        }
+
+        for (int element : arr ){
+            if (element == target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
