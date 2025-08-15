faf6dd37b92960796d9d263226626a2e0a23de5f
Srujal Sau <srujalsau@gmail.com>
2025-08-15 21:14:33 +0530

    refactor: enhance string search functionality with additional search method

--- linear_search_string.java
+++ linear_search_string.java
@@ -1,8 +1,26 @@
+// import java.util.Arrays;
+
 public class linear_search_string {
     public static void main(String[] args) {
         String name = "srujal";
         char target = 'k';
         System.out.println(search(name, target));
+        // convert string into arrays of character
+        // System.out.println(Arrays.toString(name.toCharArray()));
+    }
+
+    static boolean search2(String str, char target){
+        if(str.length() == 0){
+            return false;
+        }
+
+        for(char ch: str.toCharArray() ){
+            if(target == ch){
+                return true;
+            }
+        }
+
+        return false;
     }
 
     static boolean search(String str, char target){
