5220005911c1d31d19efd2876621020c19aabeb4
Srujal Sau <srujalsau@gmail.com>
2025-08-15 23:37:29 +0530

    feat: remove deprecated linear search and minimum finding classes

--- linear_search/max_in_2D_array.java
+++ linear_search/max_in_2D_array.java
@@ -9,15 +9,15 @@ public class max_in_2D_array {
         }
 
     static int minintwoDArray(int [][] arr){
-        int min =arr[0][0]; // Interger.MAX_VALUE;
+        int max =arr[0][0]; // Interger.MAX_VALUE;
         for( int i=0; i<arr.length; i++){
             for (int j=0; j<arr[i].length; j++){
-                if(arr[i][j] < min){
-                    min = arr[i][j];
+                if(arr[i][j] > max){
+                    max = arr[i][j];
                 }
             }
         }
-        return min;
+        return max;
     }   
 
 }
