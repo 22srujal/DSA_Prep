dd73cdb7a84962f81709ce5ff0651b388136d857
Srujal Sau <srujalsau@gmail.com>
2025-08-15 23:33:58 +0530

    feat: implement max_in_2D_array class with functionality to find minimum in a 2D array

---
+++ max_in_2D_array.java
@@ -0,0 +1,23 @@
+public class max_in_2D_array {
+    public static void main(String[] args) {
+            int [][] arr = {
+                {22,43,12,5},
+                {12,31,78,10},
+                {56,7,17}
+            };
+            System.out.println(minintwoDArray(arr));
+        }
+
+    static int minintwoDArray(int [][] arr){
+        int min =arr[0][0]; // Interger.MAX_VALUE;
+        for( int i=0; i<arr.length; i++){
+            for (int j=0; j<arr[i].length; j++){
+                if(arr[i][j] < min){
+                    min = arr[i][j];
+                }
+            }
+        }
+        return min;
+    }   
+
+}
