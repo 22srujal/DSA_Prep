cce5aec0d3f05001f976bf318d4948a4dd2eafaa
Srujal Sau <srujalsau@gmail.com>
2025-08-17 01:40:11 +0530

    feat: add Ceiling_of_a_number class to find the ceiling of a number in a sorted array

---
+++ Binary_search/Ceiling_of_a_number.java
@@ -0,0 +1,37 @@
+package Binary_search;
+
+public class Ceiling_of_a_number {
+    public static void main(String[] args) {
+        int [] arr1 = {2,3,5,9,14,16,18};
+        int target = 17;
+        System.out.println(ceilingNumber(arr1, target));
+    }
+
+    static int ceilingNumber(int [] arr, int target){
+       int start = 0;
+        int end = arr.length-1;
+        
+        while(start <= end){
+            // find the middle element
+            //int mid = (start + end)/2; // might be possible that (start + end) excceed the integer range 
+            int mid = start + (end - start)/2; 
+            
+            if(target < arr[mid]){  
+                end = mid - 1;
+            }
+            else if(target > arr[mid]){  
+                start = mid + 1;
+            }
+            else{
+                return mid;  
+            }
+        }
+        if(start>end){
+            return arr[start];
+        }
+        else{
+
+            return -1;    
+        }
+    }
+}
